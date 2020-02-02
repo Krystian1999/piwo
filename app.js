49201ba0ce09670bf5e1e25814c3078e1dee2da3
Krystian1999 <krystian.g1999@wp.pl>
2020-02-03 00:28:01 +0100

    3 i 4

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const fn1 = () => {
     var str = document.getElementById("txt1").value;
     alert("Wpisałeś " +str);
+    return fetch(`https://api.punkapi.com/v2/beers `)
+.then(res => res.json())
+.then(piwo => console.log(piwo))
+}
