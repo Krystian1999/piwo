68a065a3e4c2a5c52199178bd5ac3992d3abc3cb
Krystian1999 <krystian.g1999@wp.pl>
2020-02-05 02:01:18 +0100

    5

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+
+
 const fn1 = () => {
-    var str = document.getElementById("txt1").value;
-    alert("Wpisałeś " +str);
-    return fetch(`https://api.punkapi.com/v2/beers `)
+    //var str = document.getElementById("txt1").value;
+    //alert("Wpisałeś " +str);
+
+
+     fetch(`https://api.punkapi.com/v2/beers `)
 .then(res => res.json())
-.then(piwo => console.log(piwo))
+.then((data) => {
+    let output ='<h2>tagline</h2>';
+    data.forEach(piwo => {
+        output +=`
+        <ul>
+        <li>name: ${piwo.name} </li>
+        <li>tagline: ${piwo.tagline} </li>
+        </ul>
+        `
+        document.getElementById('output').innerHTML = output
+ }); 
+ 
+})
 }
+//.then(piwo => console.log(piwo))
