17d33da8e093db73aa303a281675230b9f6f76a8
Krystian1999 <krystian.g1999@wp.pl>
2020-02-20 21:42:34 +0100

    destructuring

--- app.js
+++ app.js
@@ -1,24 +1,69 @@
 
+const fn2 = () => {
+    const str = document.getElementById("txt1").value;
+ 
+      data = fetch(`https://api.punkapi.com/v2/beers/?beer_name=${str}` )
+     .then(res => res.json())
+     .then((data) => {
+     
+     const {name,tagline,first_brewed,description} = data[0];
+     let output ='<h2>Opis</h2>';
+     output +=`
+        <table>
+   <tr>
+      <td>name: ${name}</td> 
+   </tr>
+   <tr>
+      <td>tagline: ${tagline}</td> 
+   </tr>
+   <tr>
+      <td>first brewed: ${first_brewed}</td> 
+   </tr>
+   <tr>
+      <td>first brewed: ${description}</td> 
+   </tr>
+</table>
+      `
+      document.getElementById('output').innerHTML = output
+     console.log(`name = ${name}`);
+     console.log(`tagline = ${tagline}`);
+     console.log(`first brewed = ${first_brewed}`);
+     console.log(`description = ${description}`);  
+   }
+     )} ;
 
-const fn1 = () => {
-    //var str = document.getElementById("txt1").value;
-    //alert("Wpisałeś " +str);
 
 
-     fetch(`https://api.punkapi.com/v2/beers `)
-.then(res => res.json())
-.then((data) => {
-    let output ='<h2>tagline</h2>';
-    data.forEach(piwo => {
-        output +=`
-        <ul>
-        <li>name: ${piwo.name} </li>
-        <li>tagline: ${piwo.tagline} </li>
-        </ul>
+     /*const fn2 = () => {
+      const str = document.getElementById("txt1").value;
+   
+        data = fetch(`https://api.punkapi.com/v2/beers/?beer_name=${str}` )
+       .then(res => res.json())
+       .then((data) => {
+       
+       const {name,tagline,first_brewed,description} = data[0];
+       let output ='<h2>Opis</h2>';
+       output +=`
+          <table>
+     <tr>
+        <td>name: ${name}</td> 
+     </tr>
+     <tr>
+        <td>tagline: ${tagline}</td> 
+     </tr>
+     <tr>
+        <td>first brewed: ${first_brewed}</td> 
+     </tr>
+     <tr>
+        <td>first brewed: ${description}</td> 
+     </tr>
+  </table>
         `
         document.getElementById('output').innerHTML = output
- }); 
- 
-})
-}
-//.then(piwo => console.log(piwo))
+       console.log(`name = ${name}`);
+       console.log(`tagline = ${tagline}`);
+       console.log(`first brewed = ${first_brewed}`);
+       console.log(`description = ${description}`);  
+     }
+       )} ;
+       */
