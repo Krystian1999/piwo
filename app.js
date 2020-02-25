fdbab35b09c4804feacc62417d2353b371513986
Krystian1999 <krystian.g1999@wp.pl>
2020-02-25 13:07:38 +0100

    async/await

--- app.js
+++ app.js
@@ -1,45 +1,10 @@
-
-const fn2 = () => {
-    const str = document.getElementById("txt1").value;
- 
-      data = fetch(`https://api.punkapi.com/v2/beers/?beer_name=${str}` )
-     .then(res => res.json())
-     .then((data) => {
-     
-     const {name,tagline,first_brewed,description} = data[0];
-     let output ='<h2>Opis</h2>';
-     output +=`
-        <table>
-   <tr>
-      <td>name: ${name}</td> 
-   </tr>
-   <tr>
-      <td>tagline: ${tagline}</td> 
-   </tr>
-   <tr>
-      <td>first brewed: ${first_brewed}</td> 
-   </tr>
-   <tr>
-      <td>first brewed: ${description}</td> 
-   </tr>
-</table>
-      `
-      document.getElementById('output').innerHTML = output
-     console.log(`name = ${name}`);
-     console.log(`tagline = ${tagline}`);
-     console.log(`first brewed = ${first_brewed}`);
-     console.log(`description = ${description}`);  
-   }
-     )} ;
-
-
-
-     /*const fn2 = () => {
+      const fn2 = async () => {
       const str = document.getElementById("txt1").value;
+      const api = `https://api.punkapi.com/v2/beers/?beer_name=`;
    
-        data = fetch(`https://api.punkapi.com/v2/beers/?beer_name=${str}` )
-       .then(res => res.json())
-       .then((data) => {
+        
+        const response = await fetch(`${api}${str}` )
+        const data = await response.json();
        
        const {name,tagline,first_brewed,description} = data[0];
        let output ='<h2>Opis</h2>';
@@ -59,11 +24,7 @@ const fn2 = () => {
      </tr>
   </table>
         `
-        document.getElementById('output').innerHTML = output
-       console.log(`name = ${name}`);
-       console.log(`tagline = ${tagline}`);
-       console.log(`first brewed = ${first_brewed}`);
-       console.log(`description = ${description}`);  
+        document.getElementById('output').innerHTML = output    
      }
-       )} ;
-       */
+        
+       
